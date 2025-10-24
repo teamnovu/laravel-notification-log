2f606891dbd949f620f0ccc0ccc38e7214c12ce3
Oliver Kaufmann <oliver.kaufmann@sanio.ch>
2025-10-24 18:23:56 +0200

    chore: cleanup db json checks

--- tests/Feature/Loggers/NotificationLoggerTest.php
+++ tests/Feature/Loggers/NotificationLoggerTest.php
@@ -12,7 +12,6 @@ use Okaufmann\LaravelNotificationLog\Tests\Support\DummyNotification;
 use Okaufmann\LaravelNotificationLog\Tests\Support\DummyNotificationResendable;
 use Okaufmann\LaravelNotificationLog\Tests\Support\DummyNotificationViaTestChannel;
 use Okaufmann\LaravelNotificationLog\Tests\Support\DummyNotificationWithExtraData;
-
 use function Pest\Laravel\assertDatabaseCount;
 use function Pest\Laravel\assertDatabaseHas;
 use function Pest\Laravel\freezeTime;
@@ -83,8 +82,8 @@ it('can update a notification once it is sent', function () {
         'notifiable_type' => get_class($notifiable),
         'queued' => 0,
         'channel' => 'database',
-        'message' => json_encode(['message' => 'This is just a example message.']),
-        'data' => json_encode(['response' => ['message' => 'dummy response']]),
+        'message->message' => 'This is just a example message.',
+        'data->response->message' => 'dummy response',
         'status' => NotificationDeliveryStatus::SENT,
         'attempt' => 1,
         'sent_at' => now(),
@@ -111,9 +110,7 @@ it('can log a failed notification', function () {
         'channel' => 'database',
         'message' => null,
         'status' => NotificationDeliveryStatus::FAILED,
-        'data' => json_encode([
-            'exception' => new \stdClass,
-        ], JSON_THROW_ON_ERROR),
+        'data->message' => 'Notification could not be sent!',
         'attempt' => 1,
         'sent_at' => null,
         'notification_serialized' => null,
@@ -140,14 +137,11 @@ it('does not log a failed notification twice', function () {
         'channel' => 'test',
         'message' => null,
         'status' => NotificationDeliveryStatus::FAILED,
-        'data' => json_encode([
-            'message' => 'could not send notification!',
-        ], JSON_THROW_ON_ERROR),
+        'data->message' => 'could not send notification!',
         'attempt' => 1,
         'sent_at' => null,
         'notification_serialized' => null,
     ]);
-
 });
 
 it('can log a notification sent to a anonymous notifiable', function () {
@@ -206,7 +200,8 @@ it('it also logs notification extra data', function () {
         'notifiable_id' => $notifiable->getKey(),
         'notifiable_type' => get_class($notifiable),
         'channel' => 'database',
-        'data' => json_encode(['extra' => 'data', 'response' => ['message' => 'dummy response']]),
+        'data->extra' => 'data',
+        'data->response->message' => 'dummy response',
         'notification_serialized' => null,
     ]);
 });
